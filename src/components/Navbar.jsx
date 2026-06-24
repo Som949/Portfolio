16ee0f3c06ebd8fecf22a6064d71dfdd8d6017d0
Sanskar Sharma <ssanskar174@gmail.com>
2026-06-24 11:23:13 +0530

    resume changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,7 +61,7 @@ const Navbar = () => {
   </a>
 
   <a
-    href="/resume.pdf"
+    href="/Sanskar_Resume.pdf"
     target="_blank"
     rel="noopener noreferrer"
     className="bg-[#915eff] px-5 py-2 rounded-lg text-white font-medium hover:opacity-90"
